d4ae86b4f19dee93646fd836c0c1375b47df9582
Jonny Quarta <quarta@epsitec.ch>
2021-03-25 12:12:09 +0100

    Add executeCOmmand function

---
+++ module.js
@@ -0,0 +1,17 @@
+const addon = require("./build/Release/module");
+
+function executeCommand(serviceName, cmd) {
+  const res = addon.execute_command(serviceName, cmd);
+
+  if (res === -1) {
+    throw "could not open service manager";
+  } else if (res === -2) {
+    throw `could not connect to service ${serviceName}`;
+  } else if (res === -3) {
+    throw `sending command ${cmd} to service ${serviceName} failed`;
+  } else if (res > 0) {
+    throw `sending command ${cmd} to service ${serviceName} failed with error code ${res}`;
+  }
+}
+
+module.exports.executeCommand = executeCommand;
